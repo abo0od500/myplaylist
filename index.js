92adec0d5125b88a0eb3a5e470b19c734f114103
Abdullah <alkhamsi.abdullah@gmail.com>
2018-12-20 16:33:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,9 @@ client.on("message", message => {
             for (var make in data.name) {
             if(make == playlistName){
             for (var i = 0; i < data.name[make].length; i++) {
-                setInterval(() => { message.channel.send(botname + " " + data.name[make][i].song)}, 2000);
+                setInterval(() => {
+                    message.channel.send(botname + " " + data.name[make][i].song)
+                }, 2000);
 //                 message.channel.send(botname + " " + data.name[make][i].song);
 //                 console.log(data.name[make][i].song);
                 }
